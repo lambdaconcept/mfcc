d447c1ba0861d138d9e7d89a5ded0534b4c9da83
Pierre-Olivier Vauboin <po@lambdaconcept.com>
2021-03-17 16:34:54 +0100

    core/dct_stream: rewrite testbench

--- mfcc/core/dct_stream.py
+++ mfcc/core/dct_stream.py
@@ -2,9 +2,6 @@ from nmigen import *
 from nmigen.sim import *
 from mfcc.misc import stream
 from mfcc.misc.fft import FFT
-import numpy as np
-from scipy.fftpack import *
-from scipy.fft import fft
 
 class DCTStream(Elaboratable):
     def __init__(self, width=16, nfft=16):
@@ -77,61 +74,70 @@ class DCTStream(Elaboratable):
 
         return m
 
-
-
-
 if __name__ == "__main__":
-    
-    data = [0x4000, 0x3e29, 0x8fc2, 0x018c, 0xf3ff, 0x2cba, 0x5362, 0x9555, 0xf221, 0xfcdf, 0x19b0, 0x635e, 0xa151, 0xe017, 0x0636, 0x0861]
-    N=len(data)
+    import random
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from scipy.fftpack import *
+
+    data_in = [0x4000, 0x3e29, 0x8fc2, 0x018c, 0xf3ff, 0x2cba, 0x5362, 0x9555, 0xf221, 0xfcdf, 0x19b0, 0x635e, 0xa151, 0xe017, 0x0636, 0x0861]
+    data_out = []
+    N = len(data_in)
     dut = DCTStream(nfft=N)
 
+    def sender():
+        speed = 0.25
+
+        i = 0
+        while i < len(data_in):
+            yield dut.sink.data.eq(data_in[i])
+            yield dut.sink.last.eq(i == len(data_in)-1)
+
+            if not (yield dut.sink.valid) and (random.random() < speed):
+                yield dut.sink.valid.eq(1)
+
+            yield
+
+            if (yield dut.sink.valid) and (yield dut.sink.ready):
+                i += 1
+                yield dut.sink.valid.eq(0)
+
+    def receiver():
+        speed = 0.25
+
+        yield Passive()
+        while True:
+            yield dut.source.ready.eq((yield dut.source.valid) and (random.random() < speed))
+
+            yield
+
+            if (yield dut.source.valid) and (yield dut.source.ready):
+                data_out.append((yield dut.source.data))
+
     def bench():
-        val = []
-        yield dut.source.ready.eq(1)
-        yield dut.sink.valid.eq(1)
-        for i in range(4*N):
-            yield dut.sink.data.eq(data[i//4])
-            yield dut.sink.last.eq(((1+i) % (N*4)) == 0)
-            yield; yield Delay()
-        p = 0
-        yield dut.sink.valid.eq(0)
-        yield dut.sink.last.eq(0)
-        for i in range(5000):
-            yield; yield Delay()
-            if((yield dut.source.valid)):
-                val.append(np.int16((yield dut.source.data)))
-                #print(p, (yield dut.source.data_r), (yield dut.source.data_i))
-                p = p+1
-        print("FFT DCT:")
-        print(val)
+        while len(data_out) < len(data_in):
+            yield
 
-        """
-        print("FFT scipy:")
-        newdata = np.int16(data2)
-        #print(newdata)
-        print("DONE")
-        myfft = fft(newdata)
-        v = np.array(myfft.real//N)
-        print(v.astype(int))
-
-
-        print("\n")
-        res = np.array([val[k] * 2 * np.exp(-1j*np.pi*k/(2*16)) for k in range(16)])
-        print(res.real)
-
-        print("\n")
-        """
-        tst = [np.int16(data[i]) for i in range(16)]
-        
-        
-        #print(dct(tst, norm="ortho"))
-        
-        print(dct(tst)//64)
-
-        
     sim = Simulator(dut)
     sim.add_clock(1e-6) # 1 MHz
+    sim.add_sync_process(sender)
+    sim.add_sync_process(receiver)
     sim.add_sync_process(bench)
     with sim.write_vcd("dct.vcd"):
         sim.run()
+
+    print("DCT core:")
+    dout_core = list(map(np.int16, data_out))
+    plt.plot(dout_core)
+    print(dout_core)
+    print()
+
+    print("DCT scipy:")
+    tst = list(map(np.int16, data_in))
+    # print(dct(tst, norm="ortho"))
+    dout_scipy = dct(tst) // 64
+    plt.plot(dout_scipy)
+    print(dout_scipy)
+    print()
+
+    plt.show()
