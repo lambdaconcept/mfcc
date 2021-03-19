6ababd1e3c0d9cecf5ffa3ff3481732ab873ea2a
Pierre-Olivier Vauboin <po@lambdaconcept.com>
2021-03-19 21:32:08 +0100

    software: show both audio and cepstrums

--- software/view.py
+++ software/view.py
@@ -3,22 +3,31 @@
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.io import wavfile
 
 NCEPSTRUMS = 32
 
 def main(path):
-    raw = np.fromfile(path, dtype=np.int16)
+    wav = path + ".wav"
+    mfcc = path + ".mfcc"
+
+    fig, axs = plt.subplots(2, figsize=(15,8))
+
+    sample_rate, audio = wavfile.read(wav)
+    axs[0].plot(np.linspace(0, len(audio) / sample_rate, num=len(audio)), audio)
+    axs[0].grid(True)
+
+    raw = np.fromfile(mfcc, dtype=np.int16)
     arr = np.reshape(raw, (-1, NCEPSTRUMS))
     print("cepstrum sets:", arr.shape)
 
-    plt.figure(figsize=(15,5))
-    plt.imshow(np.transpose(arr), aspect='auto', origin='lower', cmap="inferno");
+    axs[1].imshow(np.transpose(arr), aspect='auto', origin='lower', cmap="inferno");
 
     plt.show()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("usage: {} cepstrum.bin".format(sys.argv[0]))
+        print("usage: {} basename".format(sys.argv[0]))
         sys.exit(1)
 
     main(sys.argv[1])
