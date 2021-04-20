5ff4d2136c70ef9ef388ba2b578c8660208d908d
Pierre-Olivier Vauboin <po@lambdaconcept.com>
2021-04-20 20:04:03 +0200

    mlearning: manually compute conv2d, works (some errors) with quantized models

--- mlearning/calc_conv2d.py
+++ mlearning/calc_conv2d.py
@@ -6,6 +6,8 @@ import numpy as np
 import tensorflow as tf
 
 import dataset as ds
+import fixedpoint as fp
+import utils
 
 def run(tflite_file, input_data):
     # Initialize the interpreter
@@ -37,7 +39,12 @@ def show(interpreter):
     for t in interpreter.get_tensor_details():
         print(t['index'], t['name'], interpreter.get_tensor(t['index']))
 
-def my_conv2d(din, kernels, bias):
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
+def my_conv2d(din, din_details,
+              kernels, kernels_details,
+              bias, bias_details,
+              dout_details, quantized=True):
+
     nkernels, h, w, _ = kernels.shape
     print("nkernels:", nkernels)
     fig, axs = plt.subplots(nrows=1, ncols=nkernels)
@@ -48,6 +55,24 @@ def my_conv2d(din, kernels, bias):
     # XXX no padding, odd size kernel
     oh = ih - (h - 1)
     ow = iw - (w - 1)
+
+    if quantized:
+        # Scales & Zero points
+        iscale, izp = din_details["quantization"]
+        oscale, ozp = dout_details["quantization"]
+        print("zero points:", izp, ozp)
+        kscales = kernels_details["quantization_parameters"]["scales"]
+        print("kscales:", kscales)
+
+        # Populate multiplier and shift using affine quantization
+        params = utils.PopulateConvolutionQuantizationParams(
+            iscale, izp, oscale, ozp, kscales
+        )
+        per_channel_multshifts, output_act_min, output_act_max = params
+        print("per_channel_multshifts:", per_channel_multshifts)
+        print("activation min max:", output_act_min, output_act_max)
+    else:
+        izp, ozp = 0, 0
 
     dout = np.zeros((1, oh, ow, nkernels))
 
@@ -62,32 +87,50 @@ def my_conv2d(din, kernels, bias):
             for oj in range(ow):
 
                 # Conv
-                px = 0
+                acc = 0
                 for i in range(h):
                     for j in range(w):
-                        px += ivs[oi + i][oj + j] * kernel[i][j]
+                        ival = ivs[oi + i][oj + j]
+                        acc += (ival - izp) * kernel[i][j]
 
                 # Bias
-                px += bias[k]
+                acc += bias[k]
 
-                # ReLU
-                px = px if px > 0 else 0
-                dout[0][oi][oj][k] = px
+                if quantized:
+                    # Per channel multiplier
+                    output_multiplier, output_shift = per_channel_multshifts[k]
+                    acc = fp.MultiplyByQuantizedMultiplier(acc, output_multiplier, output_shift)
+                    acc += ozp
+
+                    # Activation
+                    if acc > output_act_max:
+                        acc = output_act_max
+                    if acc < output_act_min:
+                        acc = output_act_min
+
+                else:
+                    # Activation (ReLU)
+                    acc = acc if acc > 0 else 0
+
+                dout[0][oi][oj][k] = acc
 
         axs[k].imshow(kernel) # , vmin=-128, vmax=127)
 
     return dout
 
-def validate(din1, din2):
+def validate(din1, din2, quantized=True):
     nerrors = 0
-    FLT_EPSILON = 1e-5
+    if quantized:
+        EPSILON = 0
+    else:
+        EPSILON = 1e-5
     _, nrows, ncols, nchannels = din1.shape
     for k in range(nchannels):
         for i in range(nrows):
             for j in range(ncols):
                 err = abs(din1[0][i][j][k] - din2[0][i][j][k])
-                if err > FLT_EPSILON:
-                    print("Error: [{}][{}][{}]: err: {}".format(i, j, k, err))
+                if err > EPSILON:
+                    # print("Error: [{}][{}][{}]: err: {}".format(i, j, k, err))
                     nerrors += 1
     return nerrors
 
@@ -95,25 +138,38 @@ def main():
     np.set_printoptions(precision=5,
                         threshold=sys.maxsize,
                         linewidth=256)
+    # int or float ?
+    quantized = True
 
     name = "simple"
     save_path = "saved/model_" + name + "/"
-    tflite_model_quant_file = os.path.join(save_path, "model_float.tflite")
+    if quantized:
+        tflite_model_file = os.path.join(save_path, "model.tflite")
+    else:
+        tflite_model_file = os.path.join(save_path, "model_float.tflite")
 
     sample_file = "dataset/one/c37a72d3_nohash_0.mfcc"
     mfcc_binary = tf.io.read_file(sample_file)
     mfcc = ds.decode_mfcc(mfcc_binary).numpy()
 
-    interpreter = run(tflite_model_quant_file, mfcc)
+    interpreter = run(tflite_model_file, mfcc)
     # show(interpreter)
 
     # For now tensor indexes are hardcoded
-    tensors = {
-        "conv2d_in": 7,
-        "conv2d_kernels": 6,
-        "conv2d_bias": 2,
-        "conv2d_out": 8,
-    }
+    if quantized:
+        tensors = {
+            "conv2d_in": 7,
+            "conv2d_kernels": 3,
+            "conv2d_bias": 4,
+            "conv2d_out": 8,
+        }
+    else:
+        tensors = {
+            "conv2d_in": 7,
+            "conv2d_kernels": 6,
+            "conv2d_bias": 2,
+            "conv2d_out": 8,
+        }
 
     # Get Conv2D tensors
     conv2d_in       = interpreter.get_tensor(tensors["conv2d_in"])
@@ -121,17 +177,26 @@ def main():
     conv2d_bias     = interpreter.get_tensor(tensors["conv2d_bias"])
     conv2d_out      = interpreter.get_tensor(tensors["conv2d_out"])
 
+    details = interpreter.get_tensor_details()
+    conv2d_in_details = details[tensors["conv2d_in"]]
+    conv2d_kernels_details = details[tensors["conv2d_kernels"]]
+    conv2d_bias_details = details[tensors["conv2d_bias"]]
+    conv2d_out_details = details[tensors["conv2d_out"]]
+
     nkernels = len(conv2d_kernels)
     print("conv2d_bias:", conv2d_bias)
 
     # Show input image
     print("conv2d_in:", conv2d_in[0][..., 0]) # Show first input channel (from first batch)
     plt.figure()
-    plt.imshow(conv2d_in[0][..., 0]) # , vmin=-128, vmax=127)
+    plt.imshow(conv2d_in[0][..., 0]) # vmin=-128, vmax=127)
 
     # Invoke our Conv2D
-    my_out = my_conv2d(conv2d_in, conv2d_kernels, conv2d_bias)
-    print("my_out:", conv2d_out[0][..., 0]) # Show first input channel (from first batch)
+    my_out = my_conv2d(conv2d_in, conv2d_in_details,
+                       conv2d_kernels, conv2d_kernels_details,
+                       conv2d_bias, conv2d_bias_details,
+                       conv2d_out_details, quantized=quantized)
+    print("my_out:", my_out[0][..., 0]) # Show first input channel (from first batch)
     fig, axs = plt.subplots(nrows=1, ncols=nkernels)
     for i in range(nkernels):
         axs[i].imshow(my_out[0][..., i]) # , vmin=-128, vmax=127)
@@ -142,7 +207,7 @@ def main():
     for i in range(nkernels):
         axs[i].imshow(conv2d_out[0][..., i]) # , vmin=-128, vmax=127)
 
-    ret = validate(my_out, conv2d_out)
+    ret = validate(my_out, conv2d_out, quantized=quantized)
     print("validate: nerrors:", ret)
 
     plt.show()
---
+++ mlearning/utils.py
@@ -0,0 +1,49 @@
+import math
+import numpy as np
+
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/kernels/internal/quantization_util.cc
+def QuantizeMultiplier(double_multiplier):
+    if (double_multiplier == 0.):
+        return (0, 0)
+
+    q, shift = math.frexp(double_multiplier)
+    q_fixed = q * (1 << 31)
+
+    if (q_fixed == (1 << 31)):
+        q_fixed /= 2
+        shift += 1
+
+    if (shift < -31):
+        shift = 0
+        q_fixed = 0
+
+    return (np.int32(q_fixed), shift)
+
+def CalculateActivationRangeQuantizedImpl(qmin, qmax, scale, zero_point):
+    # ReLU
+    act_min = zero_point
+    act_max = qmax
+    return (act_min, act_max)
+
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/kernels/kernel_util.cc
+def CalculateActivationRangeQuantized(output_scale, output_zero_point):
+    qmin = -128 # std::numeric_limits<int8_t>::min();
+    qmax = 127 # std::numeric_limits<int8_t>::max();
+
+    return CalculateActivationRangeQuantizedImpl(qmin, qmax, output_scale, output_zero_point)
+
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/kernels/kernel_util.cc
+def PopulateConvolutionQuantizationParams(input_scale, input_zero_point,
+                                          output_scale, output_zero_point,
+                                          filter_scales):
+    # Populate multiplier and shift using affine quantization.
+    per_channel_multshifts = []
+
+    nfilters = len(filter_scales)
+    for i in range(nfilters):
+        effective_output_scale = input_scale * filter_scales[i] / output_scale
+        mult, shift = QuantizeMultiplier(effective_output_scale)
+        per_channel_multshifts.append((mult, shift))
+
+    act_min, act_max = CalculateActivationRangeQuantized(output_scale, output_zero_point)
+    return per_channel_multshifts, act_min, act_max
